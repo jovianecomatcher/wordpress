c1f50af9db53c0982afbc03246cf0b0c00e409c1
jovianecomatcher <46739128+jovianecomatcher@users.noreply.github.com>
2019-05-17 12:07:31 +0700

    Create test.php

---
+++ test.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: EcoMatcher ShortCodes
+Plugin URI: https://www.ecomatcher.com
+Description: The plugin provides shortcodes used by EcoMatcher
+Version: 1
+Author: Maarten Swemmer, Alvin Junianto
+Bitbucket Plugin URI:
+*/
